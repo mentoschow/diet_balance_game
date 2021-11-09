a69c2c07ede9fa30f2786d20c065aa0f13f90a75
mentoschow <75648530+mentoschow@users.noreply.github.com>
2021-11-09 17:11:07 +0900

    add files
    make some changes

--- Diet Balance Game/Assets/Scripts/SampleScene/SelectCharacter.cs
+++ Diet Balance Game/Assets/Scripts/SampleScene/SelectCharacter.cs
@@ -1,26 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SelectCharacter : MonoBehaviour
 {
     public bool isBoy;
-
-    void Start()
-    {
-        GameObject.DontDestroyOnLoad(gameObject);
-    }
+    public Canvas select;
 
     public void SelectBoy()
     {
         isBoy = true;
-        SceneManager.LoadScene(2);
+        select.enabled = false;
     }
 
     public void SelectGirl()
     {
         isBoy = false;
-        SceneManager.LoadScene(2);
+        select.enabled = false;
     }
 }
