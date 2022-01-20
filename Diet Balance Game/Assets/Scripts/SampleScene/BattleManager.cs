442956ca7606fed099593c13ad8c4313635ca46d
Reiya Katsuragi <cv_katsuragi@icloud.com>
2022-01-20 20:39:38 +0900

    update battle canvas

--- Diet Balance Game/Assets/Scripts/SampleScene/BattleManager.cs
+++ Diet Balance Game/Assets/Scripts/SampleScene/BattleManager.cs
@@ -20,6 +20,7 @@ public class BattleManager : MonoBehaviour
 
     public AssetConfig enem;
     public AssetConfig character;
+    public AssetConfig probtext;
 
     //child object1
     public Image hero;
@@ -32,6 +33,8 @@ public class BattleManager : MonoBehaviour
     public Image basePentagon;
     public Button fightButton;
     public Button goawayButton;
+    public Image Winrate1;
+    public Image Winrate2;
     GameObject paramPentagon;
 
     //Flag
@@ -48,8 +51,8 @@ public class BattleManager : MonoBehaviour
     Vector3 startPos;
     Vector3 endPos;
     Vector3 present_pos;
-    float speed = 1.0F;
-    float distance_two; //distance two point
+    float speed = 300.0F;
+    float distance_two;     //distance two point
     float time_counter = 0;
 
     void Start()
@@ -69,13 +72,13 @@ public class BattleManager : MonoBehaviour
         goawayButton.gameObject.SetActive(false);
 
         //Background alpha
-        backimg01_alpha = 255f;
+        backimg01_alpha = 1.0f;
         backimg02_alpha = 0f;
         alphaSpeed = 0.001f;
 
         //enemy position
-        startPos = new Vector3(350, 0, 0);
-        endPos = new Vector3(-350, 0, 0);
+        startPos = new Vector3(990, 360, 0);
+        endPos = new Vector3(290, 360, 0);
         present_pos = startPos;
         distance_two = Vector3.Distance(startPos, endPos);
     }
@@ -102,7 +105,11 @@ public class BattleManager : MonoBehaviour
                 {
                     SceneAnimation();
                 }
-                BattleScene02();
+                else
+                {
+                    BattleScene02();
+                }
+                
             }   
         }
     }
@@ -117,10 +124,6 @@ public class BattleManager : MonoBehaviour
 
             //deactive
             hero.gameObject.SetActive(false);
-            //active(Scene2)
-            basePentagon.gameObject.SetActive(true);
-            fightButton.gameObject.SetActive(true);
-            goawayButton.gameObject.SetActive(true);
         }
     }
 
@@ -135,11 +138,12 @@ public class BattleManager : MonoBehaviour
             next = true;
             Debug.Log("Battle" + battle_result);
             battle_button_flag = false;
+            Initilized_BattleMng();
         }
 
         if (goaway_button_flag)
         {
-            
+            Initilized_BattleMng();
         }
     }
 
@@ -153,7 +157,7 @@ public class BattleManager : MonoBehaviour
         enemyObj.transform.position = Vector3.Lerp(startPos, endPos, present_Location);
         present_pos = Vector3.Lerp(startPos, endPos, present_Location);
 
-        if (alphaSpeed < 255f)
+        if (backimg02_alpha < 1.0f)
         {
             backimg01_alpha -= alphaSpeed;
             backimg02_alpha += alphaSpeed;
@@ -165,6 +169,11 @@ public class BattleManager : MonoBehaviour
         {
             //move Scene2
             sceneAnimeFlag = false;
+            //active(Scene2)
+            basePentagon.gameObject.SetActive(true);
+            paramPentagon.SetActive(true);
+            fightButton.gameObject.SetActive(true);
+            goawayButton.gameObject.SetActive(true);
         }
         
     }
@@ -173,34 +182,27 @@ public class BattleManager : MonoBehaviour
     {
         bool bool_result = false;
 
-        int probability = 80;
+        int probability;
         int random_prob = 0;
 
-        //ID‚©‚ç“G‚Ìî•ñ“Ç‚Ýž‚Ý
-        //int enemyID = enem_id;
-        //string name = enemyData[enem_id][1];
-        //string address = enemyData[enem_id][2];
-        //int energy = int.Parse(enemyData[enem_id][3]);
-        //float carb = float.Parse(enemyData[enem_id][4]);
-        //float lipid = float.Parse(enemyData[enem_id][5]);
-        //float protein = float.Parse(enemyData[enem_id][6]);
-        //float vitamin = float.Parse(enemyData[enem_id][7]);
-        //float mineral = float.Parse(enemyData[enem_id][8]);
-
+        //Calculating win rate
+        probability = CalculateProb();
         random_prob = Random.Range(0, 100);
 
+        //Battle result(Win or Lose)
         if (random_prob < probability)
         {
-            bool_result = true;
-            Initilized_BattleMng();
+            bool_result = true; //win
         }
         else
         {
-            bool_result = false;
-            Initilized_BattleMng();
+            bool_result = false;    //lose
         }
 
         Debug.Log(probability + ", " + random_prob);
+
+        //Load Number Image
+        SetWinRateImg(probability);
 
         return bool_result;
     }
@@ -224,6 +226,85 @@ public class BattleManager : MonoBehaviour
         enemy.sprite = enem.sprites[status];
     }
 
+    void SetWinRateImg(int winrate)
+    {
+        int text1, text2;
+
+        if(winrate < 10)
+        {
+            text1 = winrate;
+            Winrate1.sprite = probtext.sprites[text1];
+            Winrate2.gameObject.SetActive(false);
+        }
+        else
+        {
+            text1 = winrate % 10;
+            text2 = winrate / 10;
+            Winrate2.gameObject.SetActive(true);
+            Winrate1.sprite = probtext.sprites[text1];
+            Winrate2.sprite = probtext.sprites[text2];
+        }
+    }
+
+
+    //calculating a win rate
+    int CalculateProb()
+    {
+        int result_prob;
+        int win_count = 0;
+        float energy_prob, carb_prob, lipid_prob, protein_prob, vitamin_prob, mineral_prob;
+        float energy_p, carb_p, lipid_p, protein_p, vitamin_p, mineral_p;
+
+        //sum player's three selection
+        energy_p = pm.selectedFoodData1.energy + pm.selectedFoodData2.energy + pm.selectedFoodData3.energy;
+        carb_p = pm.selectedFoodData1.carb + pm.selectedFoodData2.carb + pm.selectedFoodData3.carb;
+        lipid_p = pm.selectedFoodData1.lipid + pm.selectedFoodData2.lipid + pm.selectedFoodData3.lipid;
+        protein_p = pm.selectedFoodData1.protein + pm.selectedFoodData2.protein + pm.selectedFoodData3.protein;
+        vitamin_p = pm.selectedFoodData1.vitamin + pm.selectedFoodData2.vitamin + pm.selectedFoodData3.vitamin;
+        mineral_p = pm.selectedFoodData1.mineral + pm.selectedFoodData2.mineral + pm.selectedFoodData3.mineral;
+
+        //calculate each probability
+        energy_prob = (pm.baseNut.energy - Mathf.Abs(pm.baseNut.energy - energy_p)) / pm.baseNut.energy;
+        carb_prob = (pm.baseNut.carb - Mathf.Abs(pm.baseNut.carb - carb_p)) / pm.baseNut.carb;
+        lipid_prob = (pm.baseNut.lipid - Mathf.Abs(pm.baseNut.lipid - lipid_p)) / pm.baseNut.lipid;
+        protein_prob = (pm.baseNut.protein - Mathf.Abs(pm.baseNut.protein - protein_p)) / pm.baseNut.protein;
+        vitamin_prob = (pm.baseNut.vitamin - Mathf.Abs(pm.baseNut.vitamin - vitamin_p)) / pm.baseNut.vitamin;
+        mineral_prob = (pm.baseNut.mineral - Mathf.Abs(pm.baseNut.mineral - mineral_p)) / pm.baseNut.mineral;
+
+        //the number of player's parameter that player wins
+        if(em.enemy.energy < energy_p)
+        {
+            win_count++;
+        }
+        if (em.enemy.carb < carb_p)
+        {
+            win_count++;
+        }
+        if (em.enemy.lipid < lipid_p)
+        {
+            win_count++;
+        }
+        if (em.enemy.protein < protein_p)
+        {
+            win_count++;
+        }
+        if (em.enemy.vitamin < vitamin_p)
+        {
+            win_count++;
+        }
+        if (em.enemy.mineral < mineral_p)
+        {
+            win_count++;
+        }
+
+        result_prob = (int)( (100 * (energy_prob + carb_prob + lipid_prob + protein_prob + vitamin_prob + mineral_prob) / 6) * win_count / 6 );
+
+        //initialize
+        win_count = 0;
+
+        return result_prob;
+    }
+
     void Initilized_BattleMng()
     {
         run_animation = false;
@@ -231,7 +312,7 @@ public class BattleManager : MonoBehaviour
         setCharaFlag = true;
         sceneAnimeFlag = false;
 
-        backimg01_alpha = 255f;
+        backimg01_alpha = 1.0f;
         backimg02_alpha = 0f;
 
         b_pp.Initialized_BPP();
