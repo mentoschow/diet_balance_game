3c472494797cc4f788dbd5a635697d17249a647c
zhou mengtao <408756434chow@gmail.com>
2022-01-29 17:16:30 +0900

    update

--- Diet Balance Game/Assets/Scripts/SampleScene/AVGMachine.cs
+++ Diet Balance Game/Assets/Scripts/SampleScene/AVGMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AVGMachine : MonoBehaviour
 {
@@ -19,20 +20,25 @@ public class AVGMachine : MonoBehaviour
     public PlayerManager pm;
     public Performance pf;
     public AudioSource typing_sound;
-    [Range(1,50)]
-    public float typingSpeed;
+    public Image next_img;
+    public AssetConfig tutorial_tex;
+    public Image tutorial_img;
+    public AudioSource skip_sound;
 
-    [SerializeField]
-    private int currentLine;
+    [SerializeField] private int currentLine;
     private string targetString;
-    [SerializeField]
-    private float timer;
+    [SerializeField] private float timer;
+    [SerializeField] private float typingSpeed;
+    [SerializeField] private float fadein_timer;
+    private float fadein_time = 0.8f;
 
     void Start()
     {
         state = STATE.OFF;       
         justEnter = true;
         typingSpeed = 10f;
+        fadein_timer = 0;
+        tutorial_img.color = new Color(1, 1, 1, 0);
     }
 
     void Update()
@@ -46,15 +52,49 @@ public class AVGMachine : MonoBehaviour
                     //LoadCharacter(characterSprite.sprites[0], characterSprite.sprites[1]);
                     justEnter = false;
                 }
+                fadein_timer = 0;
+                typing_sound.Stop();
                 break;
             case STATE.TYPING:
                 if (justEnter)
                 {
                     ShowUI();
-                    //LoadCharacter(characterSprite.sprites[0], characterSprite.sprites[1]);
-                    LoadContent(dialog.contents[currentLine].showCharacter, dialog.contents[currentLine].dialogText);
+                    //LoadCharacter(characterSprite.sprites[0], characterSprite.sprites[1]);                    
+                    LoadContent(dialog.contents[currentLine].showCharacter, dialog.contents[currentLine].dialogText);                    
                     timer = 0;
                     justEnter = false;
+                }
+                switch (currentLine)
+                {
+                    case 7:                        
+                        LoadTutorialImage(0);
+                        break;
+                    case 8:
+                        LoadTutorialImage(1);
+                        break;
+                    case 9:
+                        LoadTutorialImage(2);
+                        break;
+                    case 10:
+                        LoadTutorialImage(3);
+                        break;
+                    case 11:
+                        LoadTutorialImage(4);
+                        break;
+                    case 13:
+                        LoadTutorialImage(5);
+                        break;
+                    case 15:
+                        LoadTutorialImage(6);
+                        break;
+                    case 20:
+                        tutorial_img.color = new Color(1, 1, 1, 0);
+                        break;
+                }
+                UIPanel.contentText.fontSize = 60;
+                if (currentLine == dialog.contents.Count - 1)
+                {
+                    UIPanel.contentText.fontSize = 100;
                 }
                 checkTypingFinished();
                 UpdateContentString();
@@ -62,7 +102,8 @@ public class AVGMachine : MonoBehaviour
                 {
                     typing_sound.Play();
                     //Debug.Log(typing_sound.isPlaying);
-                }              
+                }
+                next_img.color = new Color(1, 1, 1, 0);
                 break;
             case STATE.PAUSED:
                 if (justEnter)
@@ -70,11 +111,37 @@ public class AVGMachine : MonoBehaviour
 
                     justEnter = false;
                 }
+                switch (currentLine)
+                {
+                    case 7:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                    case 8:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                    case 9:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                    case 10:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                    case 11:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                    case 13:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                    case 15:
+                        tutorial_img.color = new Color(1, 1, 1, 1);
+                        break;
+                }
                 if (typing_sound.isPlaying)
                 {
                     typing_sound.Stop();
                     //Debug.Log(typing_sound.isPlaying);
-                }               
+                }
+                fadein_timer = 0;
+                next_img.color = new Color(1, 1, 1, 1);
                 break;
         }
     }
@@ -87,9 +154,11 @@ public class AVGMachine : MonoBehaviour
     {
         switch (state)
         {
-            case STATE.TYPING:                
+            case STATE.TYPING:
+                typingSpeed = 60f;
                 break;
             case STATE.PAUSED:
+                typingSpeed = 15f;
                 NextLine();
                 if (currentLine >= dialog.contents.Count)
                 {
@@ -100,7 +169,7 @@ public class AVGMachine : MonoBehaviour
                 {
                     GoToState(STATE.TYPING);
                 }
-                
+
                 break;
         }
     }
@@ -167,5 +236,23 @@ public class AVGMachine : MonoBehaviour
         {
             UIPanel.LoadCharacterSprite(girl);
         }
+    }
+
+    void LoadTutorialImage(int id)
+    {        
+        fadein_timer += Time.deltaTime;
+        if(fadein_timer > fadein_time)
+        {
+            fadein_timer = fadein_time;
+        }
+        tutorial_img.sprite = tutorial_tex.sprites[id];
+        tutorial_img.color = new Color(1, 1, 1, fadein_timer / fadein_time);
+    }
+
+    public void Skip()
+    {
+        pf.next = true;
+        GoToState(STATE.OFF);
+        skip_sound.Play();
     }
 }
